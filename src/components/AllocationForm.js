6f69e5de0754bfefd8851c58445d9f15b8e96bbf
Luis Alvarez <la.alv.canab@gmail.com>
2023-01-23 01:39:27 -0500

    Updating

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining,currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -11,8 +11,12 @@ const AllocationForm = (props) => {
     const submitEvent = () => {
 
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
+                alert("The value cannot exceed remaining funds  "+currency+remaining);
                 setCost("");
+                return;
+            }
+            if(cost < 0) {
+                setCost(0);
                 return;
             }
 
@@ -64,6 +68,7 @@ const AllocationForm = (props) => {
                         type='number'
                         id='cost'
                         value={cost}
+                        min='0'
                         style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,35 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, currency, dispatch } = useContext(AppContext);
+    const setBudget = (value) => {
+        if(value > 20000) {
+            alert("The budget cannot exceed  "+currency+"20000");
+            value=20000
+            return;
+        }
+        if(value < 0) {
+            value=0;
+            return;
+        }
 
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value
+        });
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}<input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        step='10'
+                        min='0'
+                        value={budget}
+                        onChange={(event) => setBudget(event.target.value)}>
+                        </input>
+                        </span>
         </div>
     );
 };
